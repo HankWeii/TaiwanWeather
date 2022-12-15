56f81b5a91711e3f41edc5ac4d53b978934fe9cc
HankWeii <best0980276@gmail.com>
2022-12-16 02:14:42 +0800

    second commit

---
+++ src/components/Message/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PubSub from 'pubsub-js'
+import { useState, useEffect } from 'react'
+import './index.css'
+
+export default function Message() {
+    const [apiData, setApiData] = useState()
+    useEffect(()=>{
+        PubSub.subscribe('api',(_,data)=>{
+            setApiData(data)
+        })
+    },[])
+    return (
+        <div>
+            <div style={{fontSize:'80px', color:'gray'}}>
+                Taiwan
+            </div>
+            <div className='wearth'>Weather</div>
+            {
+                apiData && 
+                <div className='message'>
+                    <h2>{apiData.city}</h2>
+                    <h2 style={{color: apiData.weather.search('雨') < 0 && 'yellow' }}>{apiData.weather}</h2>
+                    <h3>降雨機率 : {apiData.rainy} %</h3>
+                    <h3 style={{color: apiData.maxTem > 28 ? 'red' : 'aqua'}}>{apiData.minTem} ~ {apiData.maxTem} °C</h3>
+                </div>
+            }
+        </div>
+        
+    )
+}
